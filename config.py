5a02da41661f6851910b84ed4f89573a465e4bf8
Caiwen612 <chaiboon0612@gmail.com>
2023-09-10 14:03:56 +0800

    Setup for template

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cyosmui4fqaa.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "taychaiboon-employee"
+customregion = "us-east-1"
+
