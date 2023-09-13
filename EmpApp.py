f13b0901d936c93731b6568943229fd4c031cd24
Caiwen612 <chaiboon0612@gmail.com>
2023-09-14 02:25:27 +0800

    :heavy_plus_sign: (App): Add the add data feature with some comment
    Add data and update read me

--- EmpApp.py
+++ EmpApp.py
@@ -1,24 +1,36 @@
-from flask import Flask, render_template, request
-from pymysql import connections
+from flask import Flask, render_template, request,url_for,redirect
+# from pymysql import connections
 import os
 import boto3
 from config import *
+import mariadb
+import sys
 
 app = Flask(__name__)
 
 bucket = custombucket
 region = customregion
 
-db_conn = connections.Connection(
-    host=customhost,
-    port=3306,
-    user=customuser,
-    password=custompass,
-    db=customdb
+# Connect to Maria DB
+try:
+    db_conn = mariadb.Connection(
+        host=customhost,
+        port=3306,
+        user=customuser,
+        password=custompass,
+        db=customdb
+    )
+    print("Success connect the MariaDB Platform/")
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
 
-)
-output = {}
-table = 'employee'
+# Disable Auto-Commit because mariadb default is auto commit
+db_conn.autocommit = False
+
+
+# output = {}
+# table = 'employee'
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -30,57 +42,40 @@ def home():
 def about():
     return render_template('www.tarc.edu.my')
 
-
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    #TODO: READ THE DATA FROM HTML, the 'emp_id' is refer to the name of the form
+    # emp_id = request.form['emp_id']
+    # first_name = request.form['first_name']
+    # last_name = request.form['last_name']
+    # pri_skill = request.form['pri_skill']
+    # location = request.form['location']
+    # emp_image_file = request.files['emp_image_file']
+   
     cursor = db_conn.cursor()
-
-    if emp_image_file.filename == "":
-        return "Please select a file"
+    insert_sql = "INSERT INTO student (student_id,student_name,student_email,student_cohort,student_programme) VALUES (?,?,?,?,?)"
+    #TODO: Replace the value with the above form 
+    value = (None,"caiwen","cw@gmail.com","c","RSF") #Insert none is because it will auto generate
 
     try:
-
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+        cursor.execute(insert_sql, value)
         db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
+        
+    except mariadb.Error as e:
+        print(f"Error insert student data: {e}")
 
     finally:
         cursor.close()
 
-    print("all modification done...")
+    #TODO: Pass back data to the html
+    emp_name = str("test")
+    print("The student data had added successfully")
     return render_template('AddEmpOutput.html', name=emp_name)
 
 
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    # app.run(host='0.0.0.0', port=80, debug=True) //Deployment
+    app.run(debug=True)
 
