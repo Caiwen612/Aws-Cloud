738c3e5155ad799e84ccc20773cab5cffd45e2a1
Caiwen612 <chaiboon0612@gmail.com>
2023-09-19 23:28:48 +0800

    Add deploment environment

--- EmpApp.py
+++ EmpApp.py
@@ -1073,7 +1073,9 @@ def logout():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=80, debug=True) //Deployment
-    app.run(debug=True)
+    if os.environ.get('FLASK_ENV') == 'production':
+        app.run(host='0.0.0.0', port=80)
+    else:
+        app.run(debug=True)
 
     
