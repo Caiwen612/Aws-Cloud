c864044eb38119ad5f8b9f80af73aee82edd9d78
Caiwen612 <chaiboon0612@gmail.com>
2023-09-20 01:54:59 +0800

    Add sudo

--- EmpApp.py
+++ EmpApp.py
@@ -56,7 +56,7 @@ def handle_webhook():
         abort(400)  # Request is not from GitHub. Abort!
     
     # If request is authenticated, pull latest changes and restart Flask
-    subprocess.call(['git', 'pull'], cwd=GITHUB_REPO_PATH)
+    subprocess.call(['sudo','git', 'pull'], cwd=GITHUB_REPO_PATH)
     subprocess.call(['sudo', 'systemctl', 'restart', 'myflaskapp'])
     
     return 'OK', 200
